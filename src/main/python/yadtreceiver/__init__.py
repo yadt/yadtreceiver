9df9da4f8f7bed83543609c4b94427fbf8dc1915
mriehl <maximilien.riehl@gmail.com>
2013-03-19 11:30:42 +0100

    initializing logging from the application since the rotation mechanism from twistd seems to be designed to suck out of the box

--- src/main/python/yadtreceiver/__init__.py
+++ src/main/python/yadtreceiver/__init__.py
@@ -32,7 +32,7 @@ import traceback
 from twisted.application import service
 from twisted.internet import reactor
 from twisted.python import log
-from twisted.python.logfile import LogFile
+from twisted.python.logfile import DailyLogFile
 
 from yadtbroadcastclient import WampBroadcaster
 
@@ -221,7 +221,8 @@ class Receiver(service.Service):
 
 
     def initialize_twisted_logging(self):
-        log.startLogging(LogFile.fromFullPath(self.configuration['log_filename']))
+        log_file = DailyLogFile.fromFullPath(self.configuration['log_filename'], maxRotatedFiles=30)
+        log.startLogging(log_file)
 
     def startService(self):
         """
