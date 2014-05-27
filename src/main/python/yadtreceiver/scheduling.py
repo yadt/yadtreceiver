70f11a4b4776401675ce9deef3b69eb1d3993467
Valentin Haenel <valentin.haenel@gmx.de>
2014-05-27 10:49:56 +0200

    adding scheduling and scheduling_tests from yadbroadcaster

---
+++ src/main/python/yadtreceiver/scheduling.py
@@ -0,0 +1,15 @@
+from __future__ import division
+
+from datetime import time, datetime, timedelta
+from math import ceil
+
+
+def _now():
+    return datetime.now()
+
+
+def seconds_to_midnight(offset_seconds=0):
+    now = _now()
+    next_midnight = datetime.combine(now, time()) + timedelta(days=1, seconds=offset_seconds)
+    datetime_to_midnight = next_midnight - now
+    return ceil(datetime_to_midnight.seconds + datetime_to_midnight.days * 24 * 3600)
---
+++ src/unittest/python/scheduling_tests.py
@@ -0,0 +1,21 @@
+import unittest
+import datetime
+
+from mock import patch
+
+from yadtreceiver import scheduling
+
+
+class TestTimeUntilMidnight(unittest.TestCase):
+
+    mock_now_return = datetime.datetime(1970, 1, 1, 23, 59)
+
+    @patch('yadtreceiver.scheduling._now')
+    def test_seconds_to_midnight_with_default_offset(self, now_mock):
+        now_mock.return_value = self.mock_now_return
+        self.assertEquals(102, scheduling.seconds_to_midnight(offset_seconds=42))
+
+    @patch('yadtreceiver.scheduling._now')
+    def test_seconds_to_midnight_with_other_offset(self, now_mock):
+        now_mock.return_value = self.mock_now_return
+        self.assertEquals(60, scheduling.seconds_to_midnight())
