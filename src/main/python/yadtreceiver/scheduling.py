d65624c25cd31f5460affba308b01f2a315d1c0c
mriehl <max@riehl.io>
2014-06-24 10:35:45 +0200

    adding a second to avoid multiple calls due to rounding

--- src/main/python/yadtreceiver/scheduling.py
+++ src/main/python/yadtreceiver/scheduling.py
@@ -12,4 +12,4 @@ def seconds_to_midnight(offset_seconds=0):
     now = _now()
     next_midnight = datetime.combine(now, time()) + timedelta(days=1, seconds=offset_seconds)
     datetime_to_midnight = next_midnight - now
-    return ceil(datetime_to_midnight.seconds + datetime_to_midnight.days * 24 * 3600)
+    return ceil(datetime_to_midnight.seconds + datetime_to_midnight.days * 24 * 3600) + 1
