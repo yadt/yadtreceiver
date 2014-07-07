50f9285ee4dcf950f154e667d56bb9896c12acd0
mriehl <max@riehl.io>
2014-07-07 09:55:18 +0200

    move to json for app status
    This resolves the HTML templating issues pointed out by @esc while
    remaining readable, without requiring jinja2.

--- src/main/python/yadtreceiver/app_status.py
+++ src/main/python/yadtreceiver/app_status.py
@@ -7,6 +7,10 @@ except ImportError:
     from io import StringIO
 
 from twisted.web import resource
+try:
+    import simplejson as json  # better performance
+except ImportError:
+    import json
 
 from yadtreceiver.psutil_wrapper import get_processes
 
@@ -20,42 +24,28 @@ class AppStatusResource(resource.Resource):
         super(type(self), self).__init__()
 
     def render_GET(self, request):
-        return STATUS_TEMPLATE.substitute(hostname=self.hostname,
-                                          running_commands=self.render_running_yadtshells())
-
-    def render_running_yadtshells(self):
-        return "\n".join(self.get_list_of_running_yadtshell_processes_spawned_by_receiver())
+        status_json = {
+        "name": "yadtreceiver on {0}".format(self.hostname),
+        "running_commands": self.get_list_of_running_yadtshell_processes_spawned_by_receiver(),
+        }
+        return json.dumps(status_json, indent=4)
 
     def get_list_of_running_yadtshell_processes_spawned_by_receiver(self):
         yadtshell_script_name = self.receiver.configuration.get("script_to_execute")
         rendered_commands = []
         yadtshell_processes = self.get_python_processes_containing(yadtshell_script_name)
         for p in yadtshell_processes:
-            arguments = p.cmdline()[2:]
+            arguments = p.cmdline()[1:]
             clean_arguments = filter(lambda arg: not arg.startswith("--tracking-id"), arguments)
 
-            rendered_commands.append("<tr><td>%s</td><td>yadtshell %s</td><td>%s</td></tr>" % (
-                                     basename(p.cwd()), " ".join(clean_arguments), p.pid))
+            rendered_commands.append({
+                                     "target": basename(p.cwd()),
+                                     "command": " ".join(clean_arguments),
+                                     "pid": str(p.pid),
+                                     })
         return rendered_commands
 
     def get_python_processes_containing(self, script_name):
         python_processes = filter(lambda p: p.name() == "python", get_processes())
         matching_processes = filter(lambda p: script_name in p.cmdline(), python_processes)
         return matching_processes
-
-
-STATUS_TEMPLATE = Template("""
-<html>
-<h1>Yadtreceiver on $hostname is operational.</h1>
-<h2>Running commands</h2>
-
-<p> <table cellspacing=15 style="border:1px solid black;">
-<tr>
-  <td><strong>Target</strong></td>
-  <td><strong>Command</strong></td>
-  <td><strong>PID</strong></td>
-</tr>
-$running_commands
-</table>
-</p>
-                           """)
--- src/unittest/python/app_status_tests.py
+++ src/unittest/python/app_status_tests.py
@@ -65,29 +65,29 @@ class AppStatusResourceTests(TestCase):
     def test_should_return_yadtshell_processes(self, processes):
         self.receiver.configuration.get.return_value = "yadtshell"
         p1 = Mock()
-        p1.cmdline.return_value = ["bar", "foo", "rebar"]
+        p1.cmdline.return_value = ["python", "yadtshell", "update", "--destroy"]
         p1.cwd.return_value = "p1_directory"
         p1.pid = 1
         p2 = Mock()
-        p2.cmdline.return_value = ["bar", "fuuuuu", "rebar"]
+        p2.cmdline.return_value = ["python", "yadtshell", "status"]
         p2.cwd.return_value = "p2_directory"
         p2.pid = 2
         processes.return_value = [p1, p2]
 
         self.assertEqual(
             self.app_status.get_list_of_running_yadtshell_processes_spawned_by_receiver(),
-            ['<tr><td>p1_directory</td><td>yadtshell rebar</td><td>1</td></tr>',
-             '<tr><td>p2_directory</td><td>yadtshell rebar</td><td>2</td></tr>'])
+            [{'command': 'yadtshell update --destroy', 'pid': '1', 'target': 'p1_directory'},
+             {'command': 'yadtshell status', 'pid': '2', 'target': 'p2_directory'}])
 
     @patch("yadtreceiver.app_status.AppStatusResource.get_python_processes_containing")
     def test_should_return_yadtshell_processes_with_stripped_tracking_id(self, processes):
         self.receiver.configuration.get.return_value = "yadtshell"
         p1 = Mock()
-        p1.cmdline.return_value = ["bar", "foo", "rebar", "--tracking-id='any-value'"]
+        p1.cmdline.return_value = ["python", "yadtshell", "update", "--reboot", "--tracking-id='any-value'"]
         p1.cwd.return_value = "p1_directory"
         p1.pid = 1
         processes.return_value = [p1]
 
         self.assertEqual(
             self.app_status.get_list_of_running_yadtshell_processes_spawned_by_receiver(),
-            ['<tr><td>p1_directory</td><td>yadtshell rebar</td><td>1</td></tr>'])
+            [{'command': 'yadtshell update --reboot', 'pid': '1', 'target': 'p1_directory'}])
