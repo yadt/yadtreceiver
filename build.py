4f217e76080a121f9e83bfb45b3b142eb4e78006
mriehl <max@riehl.io>
2014-12-16 16:12:57 +0100

    pin the autobahn version until we transition to the wamp2 branch

--- build.py
+++ build.py
@@ -44,6 +44,7 @@ def set_properties(project):
     project.depends_on('Twisted')
     project.depends_on('psutil')
     project.depends_on('yadtbroadcast-client')
+    project.depends_on('autobahn', '==0.8.15')
     project.depends_on('yadtcommons')
     project.depends_on('fysom')
 
